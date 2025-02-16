9a8fa2123483870eaac12f611099aa16acfa1c62
Takibist-No9 <yasuyuki.chinen@gmail.com>
2025-02-16 18:28:17 +0900

    modfiy get api method's second argument

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -2,11 +2,11 @@ import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
 // 購入履歴検索API
-export async function GET(
-  reqest: Request,
-  { params }: { params: { userId: string } },
-) {
-  const { userId } = await params;
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const userId = url.pathname.split("/").pop();
+
+  if (!userId) return NextResponse.json("User ID is required");
 
   try {
     const purchases = await prisma.purchase.findMany({
